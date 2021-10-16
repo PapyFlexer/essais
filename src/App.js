be2c1d3d3b91747f449e727f213b411ffa109e0e
Gilles Roquefeuil <groquefeuil@me.com>
2021-10-16 12:06:27 +0200

    début du test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
 import './App.css';
+import { Customers } from "./Customers";
+import { Dropdown } from 'react-dropdown-now';
+import 'react-dropdown-now/style.css';
+
+function handlePriceChange() {
+  console.log('prix');
+}
+
+function handleRoomChange() {
+  console.log('room');
+}
+function handleSurfaceChange() {
+  console.log('srface');
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      
+      <Dropdown
+        baseClassName="rdn"
+        className="priceCombo"
+        menu="div"
+        onChange={handlePriceChange}
+        onClose={handlePriceChange}
+        onOpen={handlePriceChange}
+        options={[
+          {
+            label: 'moins de 50K€',
+            value: 1
+          },
+          {
+            label: 'De 50 à 100K€',
+            value: 2
+          },
+          {
+            label: 'De 100 à 150K€',
+            value: 3
+          }
+
+        ]}
+      />
+
+<Dropdown
+        baseClassName="rdn"
+        className="roomCombo"
+        menu="div"
+        onChange={handleRoomChange}
+        onClose={handleRoomChange}
+        onOpen={handleRoomChange}
+        options={[
+          {
+            label: 'Studio',
+            value: 1
+          },
+          {
+            label: '2 pièces',
+            value: 2
+          },
+          {
+            label: '3 pièces',
+            value: 3
+          },
+          {
+            label: '4 pièces',
+            value: 4
+          },
+          {
+            label: '5 pièces et +',
+            value:5
+          }
+        ]}
+      />
+<Dropdown
+        baseClassName="rdn"
+        className="areaCombo"
+        menu="div"
+        onChange={handleSurfaceChange}
+        onClose={handleSurfaceChange}
+        onOpen={handleSurfaceChange}
+        options={[
+          {
+            label: 'moins de 20 m2',
+            value: 1
+          },
+          {
+            label: 'de 20 à 30m2',
+            value: 2
+          },
+          {
+            label: 'de 30 à 40m2',
+            value: 3
+          },
+          {
+            label: 'de 40 à 60m2',
+            value: 4
+          },
+          {
+            label :'de 60m2 à 100m2',
+            value:5
+          },
+          {
+            label: '+ de 100m2',
+            value: 6
+          }
+        ]}
+      />
+
+      <Customers />
+   </div>
+  )
 }
 
 export default App;
---
+++ src/Customers.js
@@ -0,0 +1,64 @@
+import React from "react";
+import "./App.css";
+import { customersData } from "./data";
+
+export const Customers = () => {
+    return (
+        <>
+          <HomePageHeader />
+            <div className="customer-container">
+            {customersData.map((data, key) => {
+                return(
+                    <div key={key}>
+                        <Customer
+                            key={key}
+                            id={data.id}
+                            firstName={data.firstName}
+                            lastName={data.lastName}
+                            phone={data.phone}
+                            email={data.email}
+                            search={data.search}
+                            budget={data.search.budget}
+                            rooms={data.search.rooms}
+                            surface={data.search.surface}
+                        />
+                    </div>
+                );
+            })}
+           </div>
+        </>
+    );
+};
+const HomePageHeader = () => {
+    return (
+      <header className="header">
+        <h2>Your Stock Tracker</h2>
+      </header>
+    );
+  };
+  
+  const Customer = ({ id, firstName, lastName, phone, email, search }) => {
+    if (!id) return <div />;
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <h4>{firstName}</h4>
+            </td>
+            <td>
+              <h4>{lastName}</h4>
+            </td>
+            <td>
+              <h5>{phone}</h5>
+            </td>
+            <td>
+              <p>{email}</p>
+            </td>
+            
+         </tr>
+        </tbody>
+      </table>
+    );
+  };
+  
